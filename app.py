3477a091f73c2aa5ae62d9123af324002eac2b6e
htr-volker <htr.volker@gmail.com>
2021-06-25 13:03:39 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# --------------MAKE CHANGES HERE----------------- #
+# Add your name in this list!
+trainees = [
+    "Harry",
+]
+# Add a food you like (or don't!) in this list!
+foods = [
+    "pizza",
+]
+# ------------------------------------------------ #
+
+@app.route('/')
+def home():
+    trainee = random.choice(trainees)
+    food = random.choice(foods)
+    return render_template("index.html", trainee=trainee, food=food)
+
+if __name__ == "__main__": app.run(host="0.0.0.0", port=5000, debug=True)
