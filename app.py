1e9ee212f4ac2b8cf94bf5ffd915b0ea291ac3a2
Mohammad H <mohammadsadp2122@gmail.com>
2021-06-25 14:24:40 +0100

    mohammad's my favourite food

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Mohammad"
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "fish & chips"
 ]
 # ------------------------------------------------ #
 
